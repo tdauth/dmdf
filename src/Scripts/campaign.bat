af3ffd7b1004bc08d93450ca4cf0ef9ee070eec3
barade <barade.barade@web.de>
2017-03-07 21:11:11 +0100

    Copy both campaigns and add notes for the release process toe the manual

--- src/Scripts/campaign.bat
+++ src/Scripts/campaign.bat
@@ -1,2 +1,4 @@
 del "E:\Warcraft III Englisch\Campaigns\TPoF09de.w3n"
 copy "E:\Projekte\dmdf\maps\releases\TPoF09de.w3n" "E:\Warcraft III Englisch\Campaigns\TPoF09de.w3n"
+del "E:\Warcraft III Englisch\Campaigns\TPoF09en.w3n"
+copy "E:\Projekte\dmdf\maps\releases\TPoF09en.w3n" "E:\Warcraft III Englisch\Campaigns\TPoF09en.w3n"
