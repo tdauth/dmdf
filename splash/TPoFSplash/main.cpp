a9bbd57c282af0a89dc8e78903952f7c4af390b1
barade <barade.barade@web.de>
2017-07-28 17:43:57 +0200

    Use the registry entry "Allow Local Files" and install all archive directories
    Using a custom MPQ with a custom .exe file created by MPQDraft
    does not work anymore with the latest version of Warcraft III.
    Use the registry entry to allow all local files and directories in the
    Warcraft III folder to be loaded.
    The just start Frozen Throne.exe.

--- splash/TPoFSplash/main.cpp
+++ splash/TPoFSplash/main.cpp
@@ -1,37 +1,63 @@
-#include <QApplication>
-#include <QMessageBox>
-#include <QProcess>
-#include <QSplashScreen>
-
+#include <QtCore>
+#include <QtGui>
+#include <QtWidgets>
 
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
 
-	const QString program = "../TPoF.exe";
-	QStringList arguments;
-	// Warcraft III's splash screen has a size of 500x400 which has to be hidden.
-	QPixmap pixmap(":/cropped-dmdf.jpg");
-	QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
-	splash.showMessage(QObject::tr("Copyright 2003 Blizzard Entertainment. All rights reserved. (Warcraft III: The Frozen Throne)"), Qt::AlignCenter | Qt::AlignBottom, QColor(0xFFCC00));
-	splash.show();
-	app.processEvents();
+	const QString REGISTRY_KEY = "HKEY_CURRENT_USER\\Software\\Blizzard Entertainment\\Warcraft III";
+	const QString REGISTRY_LOCAL_FILES_ENTRY = "Allow Local Files";
+	const QString REGISTRY_FROZEN_THRONE_ENTRY = "ProgramX";
 
-	QProcess *myProcess = new QProcess(&app);
-	myProcess->start(program, arguments);
-	myProcess->waitForFinished();
+	QString warcraft3Dir;
 
 	/*
-	while (!myProcess->waitForFinished(500))
+	 * Modify the registry entry "Allow Local Files" and enable it.
+	 * The directories of TPoF have to be in the Warcraft III directory, so they can be accessed by Warcraft III.
+	 * This should work on Windows 8 and 10 as well.
+	 * "HKEY_CURRENT_USER\Software\Blizzard Entertainment\Warcraft III" and add the Registry entry "Allow Local Files" of type DWORD (32 Bit) with the value 1.
+	 */
 	{
-		splash.repaint();
-		splash.raise();
-		app.processEvents();
+		QSettings settings(REGISTRY_KEY, QSettings::NativeFormat);
+		settings.setValue(REGISTRY_LOCAL_FILES_ENTRY, 1);
+		warcraft3Dir = settings.value(REGISTRY_FROZEN_THRONE_ENTRY).toString();
 	}
-	*/
 
-	splash.close();
+	qDebug() << "Warcraft 3 dir: " << warcraft3Dir;
+
+	if (warcraft3Dir.isEmpty() || !QFile::exists(warcraft3Dir))
+	{
+		warcraft3Dir = QFileDialog::getExistingDirectory(nullptr, QObject::tr("Choose Warcraft III directory"));
+	}
+
+	const QString program = warcraft3Dir;
+
+	if (QFile::exists(program))
+	{
+		QStringList arguments;
+		// Warcraft III's splash screen has a size of 500x400 which has to be hidden.
+		QPixmap pixmap(":/cropped-dmdf.jpg");
+		QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
+		splash.showMessage(QObject::tr("Copyright 2003 Blizzard Entertainment. All rights reserved. (Warcraft III: The Frozen Throne)"), Qt::AlignCenter | Qt::AlignBottom, QColor(0xFFCC00));
+		splash.show();
+		app.processEvents();
+
+		QProcess *myProcess = new QProcess(&app);
+		myProcess->start(program, arguments);
+		myProcess->waitForFinished();
+
+		splash.close();
+	}
+	else
+	{
+		QMessageBox::critical(0, QObject::tr("Error"), QObject::tr("Frozen Throne.exe does not exist in the chosen directory. Make sure that the registry entry \"%1\\%2\" is set properly!").arg(REGISTRY_KEY).arg(REGISTRY_FROZEN_THRONE_ENTRY));
+	}
+
+	{
+		QSettings settings(REGISTRY_KEY, QSettings::NativeFormat);
+		settings.setValue(REGISTRY_LOCAL_FILES_ENTRY, 0);
+	}
 
 	return 0;
-	//return app.exec();
 }
