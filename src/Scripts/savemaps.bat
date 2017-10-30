1b8c718ad898f91f9e21a8c39b647370836bba97
barade <barade.barade@web.de>
2017-10-30 10:04:16 +0100

    Extract map scripts and save maps with external map script files
    Otherwise, they can only be saved once and contain the generated map script
    afterwards.

--- src/Scripts/savemaps.bat
+++ src/Scripts/savemaps.bat
@@ -1,33 +1,36 @@
-SET CLIJASSHELPER="E:\Warcraft III\jasshelper\clijasshelper.exe"
-SET COMMONJ="E:\Projekte\dmdf\src\Asl\common.j"
-SET BLIZZARDJ="E:\Projekte\dmdf\src\Asl\Blizzard.j"
+SET CLIJASSHELPER="F:\Warcraft III\jasshelper\clijasshelper.exe"
+SET PROJECT_DIR="F:\Projekte"
+SET COMMONJ="%PROJECT_DIR%\dmdf\src\Asl\common.j"
+SET BLIZZARDJ="%PROJECT_DIR%\dmdf\src\Asl\Blizzard.j"
 SET OPTIONS=""
-SET DIR="E:\Projekte\dmdf\src\Scripts"
+SET DIR="%PROJECT_DIR%\dmdf\src\Scripts"
+SET WORKING_DIR="F:\Warcraft III"
 REM "--debug"
 
 REM Apparently the JassHelper cannot run parallel.
-echo Warning: Saving the maps does not necessarily update the map script depending on the current script in the map's MPQ archive!
 
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\Arena.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Arena.w3x" "%PROJECT_DIR%\dmdf\maps\Arena\war3map.j"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\TheNorth.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\TheNorth.w3x" "%PROJECT_DIR%\dmdf\maps\TheNorth\war3map.j"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\Tutorial.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Tutorial.w3x" "%PROJECT_DIR%\dmdf\maps\Tutorials\war3map.j"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\WorldMap.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\WorldMap.w3x" "%PROJECT_DIR%\dmdf\maps\WorldMap\war3map.j"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\Karte 2 - Gardonar.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Karte 2 - Gardonar.w3x" "%PROJECT_DIR%\dmdf\maps\Gardonar\war3map.j"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\Karte 2.1 - Gardonars Unterwelt.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Karte 2.1 - Gardonars Unterwelt.w3x" "%PROJECT_DIR%\dmdf\maps\GardonarsHell\war3map.j"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\Karte 3 - Holzbruck.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Karte 3 - Holzbruck.w3x" "%PROJECT_DIR%\dmdf\maps\Holzbruck\war3map.j"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\Karte 3.1 - Holzbrucks Unterwelt.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Karte 3.1 - Holzbrucks Unterwelt.w3x" "%PROJECT_DIR%\dmdf\maps\HolzbrucksUnderworld\war3map.j"
+REM cd %DIR%
+REM call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Karte 4 - Karornwald.w3x"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\Karte 4 - Karornwald.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Karte 5 - Deranor.w3x" "%PROJECT_DIR%\dmdf\maps\Deranor\war3map.j"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\Karte 5 - Deranor.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Karte 1 - Talras.w3x" "%PROJECT_DIR%\dmdf\maps\Talras\war3map.j"
 cd %DIR%
-call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% "E:\Warcraft III" "E:\Projekte\dmdf\maps\Karte 1 - Talras.w3x"
+call savemap.bat %CLIJASSHELPER% %COMMONJ% %BLIZZARDJ% %OPTIONS% %WORKING_DIR% "%PROJECT_DIR%\dmdf\maps\Credits.w3x" "%PROJECT_DIR%\dmdf\maps\Credits\war3map.j"
 pause
