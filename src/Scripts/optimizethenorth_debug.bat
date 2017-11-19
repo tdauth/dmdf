e69bd5632994fba63beee46e4bdbaff7251cbceb
barade <barade.barade@web.de>
2017-11-19 12:40:11 +0100

    Use home directory paths due to the latest patch of TFT

--- src/Scripts/optimizethenorth_debug.bat
+++ src/Scripts/optimizethenorth_debug.bat
@@ -1,2 +1,3 @@
-del "F:\Warcraft III Englisch\Maps\DMDF\TheNorth1.0.w3x"
-"F:\wc3tools\5.0wc3mapoptimizer\VXJWTSOPT.exe" --tweak "F:\Projekte\dmdf\src\Scripts\tweaks.vxtweak" "F:\Projekte\dmdf\maps\TheNorth.w3x" --do "F:\Warcraft III Englisch\Maps\DMDF\TheNorth1.0.w3x" --checkscriptstuff --exit
+set OUTPUT_FILEPATH="C:\Users\tamino\Documents\Warcraft III\Maps\DMDF\TheNorth1.0.w3x"
+del %OUTPUT_FILEPATH%
+"F:\wc3tools\5.0wc3mapoptimizer\VXJWTSOPT.exe" --tweak "F:\Projekte\dmdf\src\Scripts\tweaks.vxtweak" "F:\Projekte\dmdf\maps\TheNorth.w3x" --do %OUTPUT_FILEPATH% --checkscriptstuff --exit
